f1f8d234c7380f2baa456ebdfbfa95a8e0c4d25d
Surya Pratap <surya.pratap.pratap@walmart.com>
2023-05-12 13:25:08 +0530

    add

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,9 @@
 <?php
+//display error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "localhost";
 $username = "u918699320_rajat";
 $password = "Rajat@232";
