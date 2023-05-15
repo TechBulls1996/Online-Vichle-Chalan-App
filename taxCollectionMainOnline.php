9e89ebe01fb4f8faa116ca236523e5be32f7a20e
Surya Pratap <surya.pratap.pratap@walmart.com>
2023-05-15 15:36:53 +0530

    add

--- taxCollectionMainOnline.php
+++ taxCollectionMainOnline.php
@@ -45,7 +45,19 @@ print_r($_POST);
             <div class="ui-grid-col-6">
                 <label class="field-label resp-label-section"><label id="j_idt625" class="ui-outputlabel ui-widget field-label-mandate">Mobile No.</label>
                 </label><input id="mobileno" name="mobileno" type="text" class="ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all" autocomplete="off" maxlength="10" placeholder="SMS about payment will be sent to this number." title="SMS about payment will be sent to this number." onkeypress="return NumericOnly(event, '');" role="textbox" aria-disabled="false" aria-readonly="false">
-            </div>   
+            </div>  
+            <div class="ui-grid-col-6">
+                                    <label class="field-label resp-label-section">
+                                        <h3 class="top-space">Select Visiting State Name</h3>
+                                    </label><div id="ib_state" class="ui-selectonemenu ui-widget ui-state-default ui-corner-all" role="combobox" aria-haspopup="true" aria-expanded="false" aria-owns="ib_state_items" style="min-width: 208px;"><div class="ui-helper-hidden-accessible"><input id="ib_state_focus" name="ib_state_focus" type="text" autocomplete="off" aria-expanded="false" aria-required="true" aria-autocomplete="list" aria-activedescendant="ib_state_0" aria-describedby="ib_state_0" aria-disabled="false"></div><div class="ui-helper-hidden-accessible"><select id="ib_state_input" name="ib_state_input" tabindex="-1" aria-hidden="true" onchange="PrimeFaces.ab({s:&quot;ib_state&quot;,e:&quot;change&quot;,f:&quot;master_Layout_form&quot;,p:&quot;ib_state&quot;,u:&quot;operation_code&quot;});"><option value="-1" data-escape="true">---Select State---</option>
+                                    <option value="AN" data-escape="true">Andaman and Nicobar Islands</option><option value="AP" data-escape="true">Andhra Pradesh</option><option value="AR" data-escape="true">Arunachal Pradesh</option><option value="AS" data-escape="true">Assam</option><option value="BR" data-escape="true">Bihar</option><option value="CH" data-escape="true">Chandigarh</option><option value="CT" data-escape="true">Chhattisgarh</option><option value="DN" data-escape="true">Dadra and Nagar Haveli</option><option value="DD" data-escape="true">Daman and Diu</option><option value="DL" data-escape="true">Delhi</option><option value="GA" data-escape="true">Goa</option><option value="GJ" data-escape="true">Gujarat</option><option value="HR" data-escape="true">Haryana</option><option value="HP" data-escape="true">Himachal Pradesh</option><option value="JK" data-escape="true">Jammu and Kashmir</option><option value="JH" data-escape="true">Jharkhand</option><option value="KA" data-escape="true">Karnataka</option><option value="KL" data-escape="true">Kerala</option><option value="LD" data-escape="true">Lakshadweep</option><option value="MP" data-escape="true">Madhya Pradesh</option><option value="MH" data-escape="true">Maharashtra</option><option value="MN" data-escape="true">Manipur</option><option value="ML" data-escape="true">Meghalaya</option><option value="MZ" data-escape="true">Mizoram</option><option value="NL" data-escape="true">Nagaland</option><option value="OD" data-escape="true">Odisha</option><option value="PY" data-escape="true">Puducherry</option><option value="PB" data-escape="true">Punjab</option><option value="RJ" data-escape="true">Rajasthan</option><option value="SK" data-escape="true">Sikkim</option><option value="TN" data-escape="true">Tamil Nadu</option><option value="TS" data-escape="true">Telangana</option><option value="TR" data-escape="true">Tripura</option><option value="UP" data-escape="true">Uttar Pradesh</option><option value="UK" data-escape="true">Uttarakhand</option><option value="WB" data-escape="true">West Bengal</option>                                   
+                                </select></div><label id="ib_state_label" class="ui-selectonemenu-label ui-inputfield ui-corner-all">---Select State---</label>
+                                
+                                <div class="ui-selectonemenu-trigger ui-state-default ui-corner-right">
+                                    <span class="ui-icon ui-icon-triangle-1-s ui-c"></span>
+                                </div>
+                                </div>
+                                </div> 
             <div class="ui-grid-col-6">
                 <label class="field-label resp-label-section"><label id="j_idt628" class="ui-outputlabel ui-widget field-label-mandate">From State</label>
                 </label>
@@ -61,81 +73,7 @@ print_r($_POST);
 
                
             </div>
-            <div class="ui-grid-col-12">
-                        <div id="publicsignup" class="ui-panel ui-widget ui-widget-content ui-corner-all" data-widget="widget_publicsignup"><div id="publicsignup_header" class="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all"><span class="ui-panel-title">Select State Name for Tax Payment</span></div><div id="publicsignup_content" class="ui-panel-content ui-widget-content">
-                            <div class="ui-grid-row">
-                                <div class="ui-grid-col-1 resp-blank-height"></div>
-                                <div class="ui-grid-col-5">
-                                    <label class="field-label resp-label-section">
-                                        <h3 class="top-space">Select Visiting State Name</h3>
-                                    </label><div id="ib_state" class="ui-selectonemenu ui-widget ui-state-default ui-corner-all" role="combobox" aria-haspopup="true" aria-expanded="false"><div class="ui-helper-hidden-accessible"><input id="ib_state_focus" name="ib_state_focus" type="text" autocomplete="off" aria-expanded="false" aria-required="true"></div><div class="ui-helper-hidden-accessible"><select id="ib_state_input" name="ib_state_input" tabindex="-1" aria-hidden="true" onchange="PrimeFaces.ab({s:&quot;ib_state&quot;,e:&quot;change&quot;,f:&quot;master_Layout_form&quot;,p:&quot;ib_state&quot;,u:&quot;operation_code&quot;});"><option value="-1" data-escape="true">---Select State---</option>
-                                    <?php
-                                    $states = file_get_contents('./assets/json/states.json');
-                                    $data = json_decode($states, true);
-                                    // Access the data
-                                    foreach ($data as $state => $code) {
-                                    echo '<option value="'.$code.'" data-escape="true">'.$state.'</option>';
-                                    }
-                                    ?>
-                                   
-                                </select></div><label id="ib_state_label" class="ui-selectonemenu-label ui-inputfield ui-corner-all">&nbsp;</label>
-                                
-                                <div class="ui-selectonemenu-trigger ui-state-default ui-corner-right">
-                                    <span class="ui-icon ui-icon-triangle-1-s ui-c"></span>
-                                </div>
-                                <div id="ib_state_panel" class="ui-selectonemenu-panel ui-widget ui-widget-content ui-corner-all ui-helper-hidden ui-shadow ui-input-overlay"><div class="ui-selectonemenu-filter-container">
-                                    <input class="ui-selectonemenu-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all" id="ib_state_filter" name="ib_state_filter" type="text" autocomplete="off"><span class="ui-icon ui-icon-search"></span>
-                               </div>
-                               <div class="ui-selectonemenu-items-wrapper" style="max-height:200px">
-                               
-                               <ul id="ib_state_items" class="ui-selectonemenu-items ui-selectonemenu-list ui-widget-content ui-widget ui-corner-all ui-helper-reset" role="listbox">
-                                  
-                               <li class="ui-selectonemenu-item ui-selectonemenu-list-item ui-corner-all" data-label="---Select State---" tabindex="-1" role="option">---Select State---</li>
-                                <?php
-                                // Access the data
-                                foreach ($data as $state => $code) {
-                                echo '<li class="ui-selectonemenu-item ui-selectonemenu-list-item ui-corner-all" data-label="'.$state.'" tabindex="-1" role="option">'.$state.'</li>';
-                                }
-                                ?>
-                                
-                              </ul>
-                             </div></div></div><script id="ib_state_s" type="text/javascript">$(function(){PrimeFaces.cw("SelectOneMenu","widget_ib_state",{id:"ib_state",appendTo:"@(body)",filter:true,behaviors:{change:function(ext,event) {PrimeFaces.ab({s:"ib_state",e:"change",f:"master_Layout_form",p:"ib_state",u:"operation_code"},ext);}}});});</script>
-                                </div> 
-
-                                <div class="ui-grid-col-5">
-                                    <label class="field-label resp-label-section">
-                                        <h3 class="top-space">Service Name</h3>
-                                    </label><div id="operation_code" class="ui-selectonemenu ui-widget ui-state-default ui-corner-all" style="width:90%!important;" role="combobox" aria-haspopup="true" aria-expanded="false"><div class="ui-helper-hidden-accessible"><input id="operation_code_focus" name="operation_code_focus" type="text" autocomplete="off" aria-expanded="false" aria-required="true"></div><div class="ui-helper-hidden-accessible"><select id="operation_code_input" name="operation_code_input" tabindex="-1" aria-hidden="true"><option value="-1" data-escape="true">---Select Service Name---</option></select></div><label id="operation_code_label" class="ui-selectonemenu-label ui-inputfield ui-corner-all">&nbsp;</label><div class="ui-selectonemenu-trigger ui-state-default ui-corner-right"><span class="ui-icon ui-icon-triangle-1-s ui-c"></span></div><div id="operation_code_panel" class="ui-selectonemenu-panel ui-widget ui-widget-content ui-corner-all ui-helper-hidden ui-shadow ui-input-overlay"><div class="ui-selectonemenu-filter-container"><input class="ui-selectonemenu-filter ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all" id="operation_code_filter" name="operation_code_filter" type="text" autocomplete="off"><span class="ui-icon ui-icon-search"></span></div><div class="ui-selectonemenu-items-wrapper" style="max-height:200px"><ul id="operation_code_items" class="ui-selectonemenu-items ui-selectonemenu-list ui-widget-content ui-widget ui-corner-all ui-helper-reset" role="listbox"><li class="ui-selectonemenu-item ui-selectonemenu-list-item ui-corner-all" data-label="---Select Service Name---" tabindex="-1" role="option">---Select Service Name---</li></ul></div></div></div><script id="operation_code_s" type="text/javascript">$(function(){PrimeFaces.cw("SelectOneMenu","widget_operation_code",{id:"operation_code",appendTo:"@(body)",filter:true});});</script>
-                                </div> 
-                            </div>
-                            <div class="ui-grid-row top-space">
-                                <div class="ui-grid-col-12 center-position">
-                                    <button id="j_idt51" name="j_idt51" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-left" onclick="submitForm(e)" type="submit"><span class="ui-button-icon-left ui-icon ui-c ui-icon-seek-next"></span><span class="ui-button-text ui-c">Go</span></button>
-                                    
-                                   
-                                </div>
-                            </div></div></div><script id="publicsignup_s" type="text/javascript">$(function(){PrimeFaces.cw("Panel","widget_publicsignup",{id:"publicsignup"});});</script><br><div id="j_idt55" class="ui-panel ui-widget ui-widget-content ui-corner-all" style="font-size: 11pt;" data-widget="widget_j_idt55"><div id="j_idt55_header" class="ui-panel-titlebar ui-widget-header ui-helper-clearfix ui-corner-all"><span class="ui-panel-title">Follow these steps to initiate tax payment...</span></div><div id="j_idt55_content" class="ui-panel-content ui-widget-content">
-                            <ol>
-                                <li>  Select the state where you want to go from <font class="dialog-highlight-text">'Select State'</font> combo box.</li>
-                                <li>  Select service Name from <font class="dialog-highlight-text">'Service Name'</font> combo box.</li>
-                                <ol type="i">
-                                        <li type="i">  Select <font class="dialog-highlight-text">'VEHICLE TAX COLLECTION (OTHER STATE)'</font> in case you do not have NCR permit.</li>
-                                        <li type="i">  Select <font class="dialog-highlight-text">'VEHICLE TAX COLLECTION (NCR)'</font> in case you have NCR permit.</li>
-                                    </ol>    
-                                <li>  Click <font class="dialog-highlight-text">'Go'</font> button to open the vehicle details form.</li>
-                                <li>  Enter <font class="dialog-highlight-text">'Vehicle No.'</font> and click <font class="dialog-highlight-text">'Get Details'</font> button to fill the details.</li>
-                                <li>  Fill rest of the fields which are not filled automatically.</li>
-                                <li>  In case fields are not filled automatically then enter the details manually.</li>
-                                <li>  Click <font class="dialog-highlight-text">'Calculate Tax'</font> button to calculate the tax according to state notification.</li>
-                                <li>  Click <font class="dialog-highlight-text">'Pay Tax'</font> button to pay the calculated tax.</li>
-                                <li>  It opens the payment gateway of VAHAN.</li>
-                                <li>  Choose payment gateway and click on <font class="dialog-highlight-text">'Continue'</font> button.</li>
-                                <li>  And then follow the screen to pay tax.</li>
-                                <li>  After paying tax bank will redirect to the Checkpost application.</li>
-                                <li>  In case payment is success Checkpost application will generate the success receipt.</li>
-                                <li>  Print the receipt.</li>
-                            </ol></div></div><script id="j_idt55_s" type="text/javascript">$(function(){PrimeFaces.cw("Panel","widget_j_idt55",{id:"j_idt55"});});</script> 
-                    </div>
+            
         </div>
         <div class="ui-grid-row">
             <div class="ui-grid-col-6">
