48647994bedcc85d390a39752e1dba5f0c1a9eaf
Surya Pratap <surya.pratap.pratap@walmart.com>
2023-05-24 17:24:30 +0530

    add

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -18,19 +18,19 @@ include_once('../includes/connection.php');
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Admin Panel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="/admin/admin.php">Home
-                            <span class="visually-hidden">(current)</span>
+                        <a class="nav-link " href="/admin/admin.php">Home
+
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/login.php">user login</a>
+                        <a class="nav-link" href="/login.php">User login</a>
                     </li>
 
                 </ul>
